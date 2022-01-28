8d7f0994bcf68e14878564d74cf841dce204a54d
manoel081 <90159091+manoel081@users.noreply.github.com>
2022-01-28 10:56:44 -0300

    Update and rename Desafio 12 Nível Básico Princípios Básicos to Desafio 12 Nível Básico Princípios Básicos.java

--- Desafio 12 Nível Básico Princípios Básicos.java
+++ Desafio 12 Nível Básico Princípios Básicos.java
@@ -1,3 +1,4 @@
+/**
 DESAFIO
 Leia um caractere maiúsculo, que indica uma operação que deve ser realizada e uma matriz M[12][12]. Em seguida, calcule e mostre a soma ou 
 a média considerando somente aqueles elementos que estão acima da diagonal principal da matriz, conforme ilustrado abaixo (área verde).
@@ -21,6 +22,9 @@ Imprima o resultado solicitado (a soma ou média), com 1 casa após o ponto deci
 |4.1                 |                   |
 |...                 |                   |
 ------------------------------------------
+*/
+
+//Scripts:
 
 import java.util.Scanner;
 
