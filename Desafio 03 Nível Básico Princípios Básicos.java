7f97f4862f40bd33f393e98bc0fed1c0d8dade33
manoel081 <90159091+manoel081@users.noreply.github.com>
2022-01-28 10:36:18 -0300

    Update Desafio 03 Nível Básico Princípios Básicos.java

--- Desafio 03 Nível Básico Princípios Básicos.java
+++ Desafio 03 Nível Básico Princípios Básicos.java
@@ -1,4 +1,5 @@
-/**Desafio
+/**
+Desafio
 No tabuleiro de xadrez, a casa na linha 1, coluna 1 (canto superior esquerdo) é sempre branca e as cores das casas se alternam entre branca e preta, 
 formando o formato tão conhecido como xadrez. Dessa forma, como o tabuleiro tradicional tem oito linhas e oito colunas, a casa na linha 8, 
 coluna 8 (canto inferior direito) será também branca. Porém, neste problema, queremos ser capazes de predizer a cor independente do 
