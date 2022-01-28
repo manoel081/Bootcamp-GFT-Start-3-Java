ed71c789e3cc561913a90396b42e6f10243b875c
manoel081 <90159091+manoel081@users.noreply.github.com>
2022-01-28 10:52:31 -0300

    Update and rename Desafio 09 Nível Básico Princípios Básicos to Desafio 09 Nível Básico Princípios Básicos.java

--- Desafio 09 Nível Básico Princípios Básicos.java
+++ Desafio 09 Nível Básico Princípios Básicos.java
@@ -1,3 +1,4 @@
+/**
 Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha incorreta informada, escrever a mensagem "Senha Invalida". 
 Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002. 
 
@@ -19,7 +20,9 @@ Para cada valor lido mostre a mensagem correspondente à descrição do problema
 |----------------------------------------|
 |2002                | Acesso Permitido  |
 ------------------------------------------
+*/
 
+//Scripts:
 
 
 import java.io.IOException;
